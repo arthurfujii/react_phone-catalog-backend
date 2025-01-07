05304f933a2255df8fa1bd233228000e74759d4b
Arthur Fujii <arthurfujii@gmail.com>
2025-01-07 17:28:12 -0300

    error management to category controller

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -3,12 +3,20 @@ import categoryService from '../services/category.service.js';
 const getAll = async (req, res) => {
   const categories = await categoryService.getAll();
 
+  if (!categories) {
+    return res.status(400).send('No categories found');
+  }
+
   res.send(categories);
 };
 
 const getById = async (req, res) => {
   const { categoryId } = req.params;
   const category = await categoryService.getById(categoryId);
+
+  if (!category) {
+    return res.status(404).send('Category not found');
+  }
 
   res.send(category);
 };
