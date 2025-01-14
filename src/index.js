80059cc50e2c70bfeda96273689f699270a2aff8
arthurfujii <arthurfujii@gmail.com>
2025-01-14 17:52:09 -0300

    hostname var added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import createServer from './createServer.js';
 
 const PORT = process.env.PORT || 3005;
+const HOSTNAME = process.env.HOSTNAME || 'localhost';
 
 createServer().listen(PORT, () => {
-  console.log(`server is running at http://localhost:${PORT}`);
+  console.log(`server is running at http://${HOSTNAME}:${PORT}`);
 });
