97e30d2206c69d2614dec9d78829f1c57181d576
Arthur <147672701+arthurfujii@users.noreply.github.com>
2025-01-15 09:26:55 -0300

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,12 +1,20 @@
 import 'dotenv/config';
 import Sequelize from 'sequelize';
 
-const client = new Sequelize({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  dialect: 'postgres',
-});
+let client;
+
+if (process.env.DB_URL) {
+  client = new Sequelize(process.env.DB_URL);
+} else {
+  client = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
 
 export default client;
