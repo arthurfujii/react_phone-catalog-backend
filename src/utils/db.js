f99eef70cb0a04d472e5480f2f8ff59be60243eb
arthurfujii <arthurfujii@gmail.com>
2025-01-15 09:42:39 -0300

    update db parameters

--- src/utils/db.js
+++ src/utils/db.js
@@ -6,15 +6,13 @@ let client;
 if (process.env.DB_URL) {
   client = new Sequelize(process.env.DB_URL);
 } else {
-  client = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'postgres',
-    },
-  );
+  client = new Sequelize({
+    host: 'localhost',
+    dialect: 'postgres',
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  });
 }
 
 export default client;
