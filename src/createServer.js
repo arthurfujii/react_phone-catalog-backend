165b2f6a67d87151e2fee67f894ffd61a2b84fe0
arthurfujii <arthurfujii@gmail.com>
2025-01-15 13:41:47 -0300

    temporary script createEntries

--- src/createServer.js
+++ src/createServer.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import productRouter from './routes/product.route.js';
 import categoryRouter from './routes/category.route.js';
+import createEntries from '../createEntries.js';
 
 const app = express();
 
@@ -10,6 +11,7 @@ const createServer = () => {
   app.use(express.json());
   app.use('/products', productRouter);
   app.use('/categories', categoryRouter);
+  app.use('/init', createEntries);
 
   return app;
 };
--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -2,9 +2,11 @@ import express from 'express';
 import categoryController from '../controllers/category.controller.js';
 import catchError from '../utils/catchError.js';
 
+
 const router = express.Router();
 
 router.get('/', catchError(categoryController.getAll));
 router.get('/:categoryId', catchError(categoryController.getById));
+
 
 export default router;
